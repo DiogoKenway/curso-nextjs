5c1bfea496e344f6ee8baf9cf4006d004c823724
Diogo Alves <diogodidi2022@gmail.com>
2023-01-04 02:04:32 -0300

    adding file

---
+++ pages/basico/lista1.jsx
@@ -0,0 +1,20 @@
+function elements(qtd) {
+    return (
+        qtd.map((el, index) => (
+            <span ke={index}>{el},</span>
+        ))
+    )
+}
+
+
+
+export default function Lista1() {
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    return (
+        <div>
+            {elements(list)}
+        </div>
+    )
+}
+

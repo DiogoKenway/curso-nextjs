c69aa223331bd899244ffe17224b9c7bb9820fe4
Diogo Alves <diogodidi2022@gmail.com>
2023-01-05 21:49:08 -0300

    adding new file "Fragment1"

---
+++ pages/basico/fragment1.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react"
+
+export default function fragment() {
+    return (
+        <Fragment>
+            <h1>Titulo</h1>
+            <h2>Subtitulo</h2>
+        </Fragment>
+        
+    )
+}

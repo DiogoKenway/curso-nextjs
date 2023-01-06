ac23d5a04234deae8ca5e7a6cdf70a1b9562b060
Diogo Alves <diogodidi2022@gmail.com>
2023-01-05 23:50:00 -0300

    integration with css in NextJs

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,12 @@
+// Não posso importar aqui!
+// O import foi feito dentro de _app.jsx;
+
+export default function integracao() {
+    return (
+        <div className="integracao1">
+            <div className="vermelho">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <div className="branco">Texto #03</div>
+        </div>
+    )
+}

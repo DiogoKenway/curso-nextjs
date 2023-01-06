d85ee66519ff2601ab4ea2054b861a35c13f88cb
Diogo Alves <diogodidi2022@gmail.com>
2023-01-05 23:10:22 -0300

    changing files and adding properties

--- pages/basico/usandoTitulo.jsx
+++ pages/basico/usandoTitulo.jsx
@@ -2,6 +2,16 @@ import Titulo from "../../components/Titulo";
 
 export default function usandoTitulo() {
     return (
-        <Titulo />
+        <>
+            <Titulo 
+                principal="Página de cadastro"
+                secundario="Incluir, alterar e excluir coisas!"
+            />
+            <Titulo 
+                principal="Página de login"
+                secundario="Informe o seu email e senha!"
+                pequeno
+            />
+        </>
     )
 }

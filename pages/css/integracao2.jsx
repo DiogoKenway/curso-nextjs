adb0feafd00ecf81e4d81823d5e7fcaba4c4ebf1
Diogo Alves <diogodidi2022@gmail.com>
2023-01-06 00:25:31 -0300

    integration with css in NextJs again in a new file

--- pages/css/integracao2.jsx
+++ pages/css/integracao2.jsx
@@ -1,10 +1,11 @@
+import styles from "./integracao2.module.css";
 
 export default function integracao2() {
     return (
-        <div className="integracao1">
-            <div className="vermelho">Texto #01</div>
-            <div className="azul">Texto #02</div>
-            <div className="branco">Texto #03</div>
+        <div id={styles.integracao2}>
+            <div className={styles.vermelho}>Texto #01</div>
+            <div className={styles.azul}>Texto #02</div>
+            <div className={styles.branco}>Texto #03</div>
         </div>
     )
 }

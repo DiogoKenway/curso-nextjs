06743d051741b122becd567042bdc2e0d1ea6840
Diogo Alves <diogodidi2022@gmail.com>
2023-01-07 00:44:51 -0300

    adding code in the file "Estilo, global e usando"

--- components/Estilo.jsx
+++ components/Estilo.jsx
@@ -1,8 +1,19 @@
 export default function Estilo(props) {
+
+    const aplicarClasse = props.numero >= 0 ? "azul" : "vermelho";
+
     return (
         <div>
-            <h1>Texto</h1>        
+            <h1 style={ { 
+                    background: props.numero >= 0 ? "#2D8" : "#D20259",
+                    color: props.color,
+                    textAlign: props.direita ? "right" : "left"
+                 } }>
+                Texto
+            </h1>
+            <h2 className={aplicarClasse}>
+                h2
+            </h2>  
         </div>
-
     )
 }
--- pages/css/usandoEstilo.jsx
+++ pages/css/usandoEstilo.jsx
@@ -1,3 +1,10 @@
+import Estilo from "../../components/Estilo";
+
 export default function usandoEstilo() {
-    return <h1>Estilo</h1>
+    return (
+        <>
+            <Estilo numero={1} color="#000" direita />
+            <Estilo numero={-1} color="#F1f1f1" />
+        </>
+    )
 }

6b5352f7581121f8c9542c556de67a3510fb5ace
Diogo Alves <diogodidi2022@gmail.com>
2023-01-08 23:44:08 -0300

    adding code in file

--- pages/render/repeticao.jsx
+++ pages/render/repeticao.jsx
@@ -1,9 +1,20 @@
 export default function Repeticao1() {
+
+    const liastaAprovados = ["João", "Maria", "Ana", "Bia", "Carlos", "Daniel", "Laura"];
+
+    function renderizarLista() {
+        return (
+            <>
+                <li>Elemento #01</li>
+                <li>Elemento #02</li>
+                <li>Elemento #03</li>            
+            </>
+        )
+    }
+
     return(
         <ul>
-            <li>Elemento #01</li>
-            <li>Elemento #02</li>
-            <li>Elemento #03</li>
+            {renderizarLista()}
         </ul>
     )
 }

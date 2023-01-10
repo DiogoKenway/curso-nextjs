02b8df750b1cbf2c0a25f3559000203f5a1855ef
Diogo Alves <diogodidi2022@gmail.com>
2023-01-09 23:38:52 -0300

    adding constructor in class

--- model/prouto.js
+++ model/prouto.js
@@ -1,3 +1,11 @@
 class Produto {
-    
+    #id;
+    #nome;
+    #preco;
+
+    constructor(id, nome, preco) {
+        this.#id = id;
+        this.#nome = nome;
+        this.#preco = preco;
+    }
 }

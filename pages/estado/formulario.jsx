80d3ccef89c249a4d72926b9157d677aca2eb7ab
Diogo Alves <diogodidi2022@gmail.com>
2023-01-13 00:57:05 -0300

    finalizing controlled component

---
+++ pages/estado/formulario.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+export default function formulario() {
+
+    const [valor, setValor] = useState("")
+
+    function alterarInput() {
+        setValor(valor + "!")
+    }
+
+    return (
+        <div style={{display: "flex",
+            flexDirection: "column",
+            width: "400px"}}>
+            <input type="text" value={valor} 
+                onChange={e => setValor(e.target.value)} />
+                <span>{valor}</span>
+            <button onClick={alterarInput}>Alterar</button>
+        </div>
+    )
+}

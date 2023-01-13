4bea5857d6d8ba386782046d6f2a7e2ed043ef7b
Diogo Alves <diogodidi2022@gmail.com>
2023-01-12 23:55:12 -0300

    finalizing state components

--- pages/estado/mouse.jsx
+++ pages/estado/mouse.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 export default function mouse(props) {
+
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const estilo = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#333",
+        color: "#FFF",
+        height: "100vh",
+        gap: "10px"
+    }
+
+
+    function quandoMouseMover(event) {
+        setX(event.clientX);
+        setY(event.clientY);
+    }
+
     return (
-        <div style={{
-            background: "#333",
-            color: "#FFF"
-        }}>
-            mouse
+        <div style={estilo} onMouseMove={quandoMouseMover}>
+            <span>Eixo X: {x}</span>
+            <span>Eixo Y: {y}</span>
         </div>
     )
 }

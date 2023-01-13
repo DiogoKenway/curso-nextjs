a7f07b8d58560cd2152beceae03192e77a17102b
Diogo Alves <diogodidi2022@gmail.com>
2023-01-12 22:14:21 -0300

    adding different ways to call functions in react

--- pages/basico/botao.jsx
+++ pages/basico/botao.jsx
@@ -3,7 +3,19 @@ function acao1() {
 }
 
 export default function Botao() {
+
+    function acao2() {
+        console.log("acao2");
+    }
+
     return (
-        <button>Click #01</button>
+        <>
+            <button onClick={acao1}>Click #01</button>
+            <button onClick={acao2}>Click #02</button>
+            <button onClick={function () {
+                console.log("acao3")
+            }}>Click #03</button>
+            <button onClick={() => console.log("acao4")}>Click #04</button>
+        </>
     )
 }

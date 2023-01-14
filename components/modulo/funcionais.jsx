d54bcde9ce3aef1b8c8fd11e18c637572b097b16
Diogo Alves <diogodidi2022@gmail.com>
2023-01-14 19:33:19 -0300

    addin new file

---
+++ components/modulo/funcionais.jsx
@@ -0,0 +1,7 @@
+export function Com1() {
+    return <h1>Comp1</h1>
+}
+
+export const Comp2 = function() {
+    return <h2>Comp2</h2>
+}
---
+++ pages/modulo/teste.jsx
@@ -0,0 +1,7 @@
+import { Com1, Comp2 } from "../../components/modulo/funcionais";
+
+export default function teste() {
+    return (
+        <div></div>
+    )
+}
